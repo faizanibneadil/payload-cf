45b84709bcb69d7260ac851b6d2b211dfa87a49f
Faizan Adil <107530101+faizanibneadil@users.noreply.github.com>
2026-09-08 13:00:19 +0500

    gpt nextjs config changes recomondation

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig = {
   },
   // Packages with Cloudflare Workers (workerd) specific code
   // Read more: https://opennext.js.org/cloudflare/howtos/workerd
-  serverExternalPackages: ['jose', 'pg-cloudflare'],
+  serverExternalPackages: ['jose', 'pg-cloudflare','drizzle-kit'],
 
   // Your Next.js config here
   webpack: (webpackConfig: any) => {
